527d5f74db8429c7ad8d257e9554862e0c1fead7
MedRedha93 <redha.dirac@gmail.com>
2019-12-31 01:54:55 +0100

    Adding an animated button.js script

---
+++ js/fancy-button.js
@@ -0,0 +1,17 @@
+$(".hero-btn").click(function() {
+  if (
+    !$(".hero-btn")
+      .parent()
+      .hasClass("active")
+  ) {
+    $(this)
+      .parent()
+      .stop()
+      .addClass("active");
+    setTimeout(function() {
+      $(".hero-btn")
+        .parent()
+        .removeClass("active");
+    }, 1500);
+  }
+});
