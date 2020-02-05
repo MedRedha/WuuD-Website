ba4f7c35dd70c26774ae407f9c2a95d48fd2fbc0
Med Redha <redha.dirac@gmail.com>
2020-02-05 01:06:38 +0100

    Upgrading version

---
+++ build/precache-manifest.ab9db1551046e32e290734258643d006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f535bcee391c54eb6a2bf4b73c3a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96dee199b345a20a3c50",
+    "url": "/static/js/2.ce154952.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ce154952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4639e24b11f294f58447",
+    "url": "/static/js/main.a9afd420.chunk.js"
+  },
+  {
+    "revision": "0e41ed54fae3bc7f8cec",
+    "url": "/static/js/runtime-main.a9dcd9a6.js"
+  }
+]);
