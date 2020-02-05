2bc6267ed62794a51164d700f875a2558f62b389
Med Redha <redha.dirac@gmail.com>
2020-02-05 01:36:23 +0100

    Prettier linter fix

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -8,7 +8,7 @@ class FooterLinks extends React.Component {
             <div className="row">
                 <div className="col-lg-12">
                     <div className="float-left pull-none ">
-                        <p className="copy-rights text-muted">{(new Date().getFullYear())} © Dorsin - Themesbrand</p>
+                        <p className="copy-rights text-muted">{(new Date().getFullYear())} © Dorsin - WuuD</p>
                     </div>
                     <div className="float-right pull-none ">
                         <img src="images/payment.png" alt="payment-img" height="36" />
