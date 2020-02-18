cdb6bc1bf260e77f0303fe61959c2dceff0c1ed7
Med Redha <redha.dirac@gmail.com>
2020-02-18 01:57:34 +0100

    configuring footer links

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
 class FooterLinks extends React.Component {
   render() {
-  	return (
-        <div className="footer-alt">
+    return (
+      <div className="footer-alt">
         <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="float-left pull-none ">
-                        <p className="copy-rights text-muted">{(new Date().getFullYear())} © Dorsin - WuuD</p>
-                    </div>
-                    <div className="float-right pull-none ">
-                        <img src="images/payment.png" alt="payment-img" height="36" />
-                    </div>
-                    <div className="clearfix"></div>
-                </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="float-left pull-none ">
+                <p className="copy-rights text-muted">
+                  {new Date().getFullYear()} © WuuD - WuuD
+                </p>
+              </div>
+              <div className="float-right pull-none ">
+                <img src="images/payment.png" alt="payment-img" height="36" />
+              </div>
+              <div className="clearfix"></div>
             </div>
+          </div>
         </div>
-    </div>
-  	);
+      </div>
+    );
   }
 }
 export default FooterLinks;
