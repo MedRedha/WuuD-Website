917a530ac06870bebabb7d34813ea20cd0956bba
Med Redha <redha.dirac@gmail.com>
2020-02-22 13:59:00 +0100

    color switcher section update

--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -5,42 +5,44 @@ import { Link } from 'react-router-dom';
 
 class Switcher extends React.Component {
   render() {
-  	return (
-        <div id="style-switcher">
-            <div>
-                <h3>Select your color</h3>
-                <ul className="pattern">
-                    <li>
-                    <Link to="JavaScript:Void(0);" className="color1" ></Link>
-                    </li>
-                    <li>
-                    <Link to="JavaScript:Void(0);"  className="color2"></Link>
-                    </li>
-                    <li>
-                       <Link to="JavaScript:Void(0);" className="color3"></Link>
-                    </li>
-                    <li>
-                       <Link to="JavaScript:Void(0);" className="color4"></Link>
-                    </li>
-                    <li>
-                        <Link to="JavaScript:Void(0);" className="color5"></Link>
-                    </li>
-                    <li>
-                       <Link to="JavaScript:Void(0);"  className="color6"></Link>
-                    </li> 
-                    <li>
-                       <Link to="JavaScript:Void(0);" className="color7"></Link>
-                    </li>
-                    <li>
-                       <Link to="JavaScript:Void(0);"  className="color8"></Link>
-                    </li>                    
-                </ul>
-            </div>
-            <div className="bottom">
-            <a href="#" className="settings"><i className="mdi mdi-settings mdi-spin"></i></a>
-            </div>
+    return (
+      <div id="style-switcher">
+        <div>
+          <h3>Select your color</h3>
+          <ul className="pattern">
+            <li>
+              <Link to="JavaScript:Void(0);" className="color1"></Link>
+            </li>
+            <li>
+              <Link to="JavaScript:Void(0);" className="color2"></Link>
+            </li>
+            <li>
+              <Link to="JavaScript:Void(0);" className="color3"></Link>
+            </li>
+            <li>
+              <Link to="JavaScript:Void(0);" className="color4"></Link>
+            </li>
+            <li>
+              <Link to="JavaScript:Void(0);" className="color5"></Link>
+            </li>
+            <li>
+              <Link to="JavaScript:Void(0);" className="color6"></Link>
+            </li>
+            <li>
+              <Link to="JavaScript:Void(0);" className="color7"></Link>
+            </li>
+            <li>
+              <Link to="JavaScript:Void(0);" className="color8"></Link>
+            </li>
+          </ul>
         </div>
-  	);
+        <div className="bottom">
+          <a href="#" className="settings">
+            <i className="mdi mdi-settings mdi-spin"></i>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 export default Switcher;
