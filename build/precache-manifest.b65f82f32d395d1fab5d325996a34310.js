7e5bb3a9c2124eeb976c5a859ea910d982952ee2
Med Redha <redha.dirac@gmail.com>
2020-02-22 14:45:14 +0100

    auto-generated build files

---
+++ build/precache-manifest.b65f82f32d395d1fab5d325996a34310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d091fb57fc2ac6a3fb91425a68708c4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc319c66192c4d455ad9",
+    "url": "/static/css/2.5ffa1744.chunk.css"
+  },
+  {
+    "revision": "fc319c66192c4d455ad9",
+    "url": "/static/js/2.2a3f37c7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2a3f37c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3a8a10d8dbbabd7e8d",
+    "url": "/static/js/main.e751c4c0.chunk.js"
+  },
+  {
+    "revision": "ad071fec86346fb3a65a",
+    "url": "/static/js/runtime-main.ed0b404b.js"
+  }
+]);
